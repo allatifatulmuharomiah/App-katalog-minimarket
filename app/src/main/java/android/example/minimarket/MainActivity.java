02e452d437ba302a66225ef9f56ba9da4a852746
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-04-22 22:58:43 +0700

    uodate tombol dashboard ke list katalog sesuai kategori

--- app/src/main/java/android/example/minimarket/MainActivity.java
+++ app/src/main/java/android/example/minimarket/MainActivity.java
@@ -1,9 +1,13 @@
 package android.example.minimarket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +19,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ConstraintLayout btn1=findViewById(R.id.btn1);
+        ConstraintLayout btn2=findViewById(R.id.btn2);
+        ConstraintLayout btn3=findViewById(R.id.btn3);
+        ConstraintLayout btn4=findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent inte = new Intent(MainActivity.this, minuman.class);
+                startActivity(inte);
+            }
         });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent inte = new Intent(MainActivity.this, buahsayur.class);
+                startActivity(inte);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent inte = new Intent(MainActivity.this, makanan.class);
+                startActivity(inte);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent inte = new Intent(MainActivity.this, lainlain.class);
+                startActivity(inte);
+            }
+        });
+
+
     }
 }
