b080fdb9ae40a010a1673d0f98103dc005246793
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-04-23 14:53:45 +0700

    Add tombol input produk

--- app/src/main/java/android/example/minimarket/inputproduk.java
+++ app/src/main/java/android/example/minimarket/inputproduk.java
@@ -1,9 +1,6 @@
 package android.example.minimarket;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class minuman extends AppCompatActivity {
+public class inputproduk extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_minuman);
+        setContentView(R.layout.activity_inputproduk);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button btn4=(Button)findViewById(R.id.input);
-
-        btn4.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                // TODO Auto-generated method stub
-                Intent inte = new Intent(minuman.this, inputproduk.class);
-                startActivity(inte);
-            }
         });
     }
 }
