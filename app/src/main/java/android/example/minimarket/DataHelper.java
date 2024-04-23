d9c1ccd4eee28e8a2b98a16ef6654391d02f5be9
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-04-23 21:41:49 +0700

    fix update data and add datahlper

---
+++ app/src/main/java/android/example/minimarket/DataHelper.java
@@ -0,0 +1,36 @@
+package android.example.minimarket;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DataHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "minuman.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DataHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE biodata(id INTEGER PRIMARY KEY, nama TEXT NULL, harga INTEGER NULL)";
+        Log.d("Data", "onCreate: " + sql);
+        db.execSQL(sql);
+
+        // Insert sample data
+        sql = "INSERT INTO biodata (id, nama, harga) VALUES ('0', 'Susu Segar', '7000')";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Handle database upgrades, if needed
+        Log.w(DataHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS biodata");
+        onCreate(db);
+    }
+}
+
