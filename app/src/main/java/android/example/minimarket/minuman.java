22eae7a0e94c4d14ab13b02c40764308c01999dc
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2024-04-23 23:28:10 +0700

    list beres, minuman beress

--- app/src/main/java/android/example/minimarket/minuman.java
+++ app/src/main/java/android/example/minimarket/minuman.java
@@ -12,6 +12,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.view.ViewGroup;
+
+
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -39,6 +43,7 @@ public class minuman extends AppCompatActivity {
             return insets;
         });
 
+        ListView01 = (ListView) findViewById(R.id.listview);
         Button btn4=(Button)findViewById(R.id.input);
 
         btn4.setOnClickListener(new View.OnClickListener() {
@@ -56,49 +61,68 @@ public class minuman extends AppCompatActivity {
         RefreshList();
     }
 
-    public void RefreshList(){
-        SQLiteDatabase db = dbcenter.getReadableDatabase();
-        cursor = db.rawQuery("SELECT * FROM minuman",null);
-        daftar = new String[cursor.getCount()];
-        cursor.moveToFirst();
-        for (int cc=0; cc < cursor.getCount(); cc++){
-            cursor.moveToPosition(cc);
-            daftar[cc] = cursor.getString(1).toString();
-        }
-        ListView01 = (ListView)findViewById(R.id.listview);
-        ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
-        ListView01.setSelected(true);
+    public void RefreshList() {
+        try {
+            SQLiteDatabase db = dbcenter.getReadableDatabase();
+            cursor = db.rawQuery("SELECT * FROM minuman", null);
+            daftar = new String[cursor.getCount()];
+            cursor.moveToFirst();
+            for (int i = 0; i < cursor.getCount(); i++) {
+                String namaProduk = cursor.getString(1); // Mengambil nama produk
+                String hargaProduk = cursor.getString(2); // Mengambil harga produk
+                daftar[i] = namaProduk; // Gabungkan nama dan harga produk
+                cursor.moveToNext();
+            }
+            cursor.moveToFirst(); // Reset cursor position to the first row
+            // Menggunakan adapter kustom dengan layout kustom
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list, R.id.listName, daftar) {
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView textView = view.findViewById(R.id.listPrice);
+                    // Move cursor to the correct position based on the list item position
+                    cursor.moveToPosition(position);
+                    String harga = cursor.getString(2); // Ambil harga produk dari cursor
+                    textView.setText("Rp " + harga); // Set teks harga
+                    return view;
+                }
+            };
+            ListView01.setAdapter(adapter);
+            ListView01.setSelected(true);
 
-        ListView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-
-            public void onItemClick(AdapterView arg0, View arg1, int arg2, long arg3) {
-                final String selection = daftar[arg2];
-//.getItemAtPosition(arg2).toString();
-                final CharSequence[] dialogitem = {"Update Data", "Hapus Data"};
-                AlertDialog.Builder builder = new
-                        AlertDialog.Builder(minuman.this);
-                builder.setTitle("Pilihan");
-                builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int item) {
-                        switch(item){
-
-                            case 0 :
-                                Intent in = new Intent(getApplicationContext(),
-                                        updatedata.class);
-                                in.putExtra("nama", selection);
-                                startActivity(in);
-                                break;
-                            case 1 :
-                                SQLiteDatabase db = dbcenter.getWritableDatabase();
-                                db.execSQL("delete from minuman where nama = '"+selection+"'");
-                                RefreshList();
-                                break;
+            ListView01.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                    final String selection = daftar[arg2];
+                    final CharSequence[] dialogitem = {"Update Data", "Hapus Data"};
+                    AlertDialog.Builder builder = new AlertDialog.Builder(minuman.this);
+                    builder.setTitle("Pilihan");
+                    builder.setItems(dialogitem, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            switch (item) {
+                                case 0:
+                                    Intent in = new Intent(getApplicationContext(), updatedata.class);
+                                    in.putExtra("nama", selection);
+                                    startActivity(in);
+                                    break;
+                                case 1:
+                                    SQLiteDatabase db = dbcenter.getWritableDatabase();
+                                    db.execSQL("delete from minuman where nama = '" + selection + "'");
+                                    RefreshList();
+                                    break;
+                            }
                         }
-                    }
-                });
-                builder.create().show();
-            }});
-        ((ArrayAdapter)ListView01.getAdapter()).notifyDataSetInvalidated();
+                    });
+                    builder.create().show();
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
+
+
+
+
 }
